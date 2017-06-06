a75f12cf4046dbb8abe80595b0a24aef3ec17a7a
Simon Humbert <shumbert@kernix.com>
2017-06-06 23:06:14 +0200

    Profil

--- index.php
+++ index.php
@@ -4,33 +4,152 @@
 		<title>Data Vizualisation - TP1</title>
 		<!-- Inclusion CSS (librairie + perso) -->
 		<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap_theme.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://blackrockdigital.github.io/startbootstrap-clean-blog/css/clean-blog.min.css">
 		<link rel="stylesheet" type="text/css" href="css/dataviz.css">
 		
 		<!-- Inclusion JS (librairie + scripts de création de graph) -->
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
-		
+		<script type="text/javascript" src="js/renderer/jqplot.dateAxisRenderer.js"></script>
+		<script type="text/javascript" src="js/renderer/jqplot.categoryAxisRenderer.js"></script>
+		<script type="text/javascript" src="js/renderer/jqplot.barRenderer.js"></script>
+		<script type="text/javascript" src="js/renderer/jqplot.pieRenderer.js"></script>
+
 		<script type="text/javascript" src="js/dataviz.js"></script>
 	</head>
 	<body>
 		<?php include ('structure/header.php'); ?>
+		<?php if(isset($_GET["user_id"])){
+			$id = $_GET["user_id"];
+		} else{
+			$id = 1;
+		} ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+					<div class="post-preview" id="profile">
+						<h2 class="post-title">
+							<?php
+							if(file_exists("img/avatar".$id.".png")){
+								echo '<img src="img/avatar'.$id.'.png">';
+							}
+							else if(file_exists("img/avatar".$id.".jpg")){
+								echo '<img src="img/avatar'.$id.'.jpg">';
+							}
+							else if(file_exists("img/avatar".$id.".gif")){
+								echo '<img src="img/avatar'.$id.'.gif">';
+							}
+							else{
+								echo '<img src="http://via.placeholder.com/100x100">';
+							}
+							?>
+							Profil de <b id="name"></b>
+						</h2>
 
-		<section>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="section-heading">Data Vizualisation - TP1</h1>
-                    <p class="lead section-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
-                </div>
-            </div>
-        </div>
-    </section>
+					</div>
+					<hr>
+					<div class="post-preview">
+
+						<form method="GET" id="form_nav" class="form-inline">
+							<div class="form-group">
+								<label for="id" style="margin-right: 10px;">
+									Visit user:
+								</label>
+								<div class="input-group">
+									<input type="number" class="form-control" name="user_id" value="<?= $id; ?>" id="user_id">
+									<div class="input-group-addon">(id)</div>
+								</div>
+								<input type="submit" class="btn btn-primary">
+							</div>
+
+						</form>
+
+					</div>
 
 
 
-		<?php include ('structure/footer.php'); ?>
+					<hr>
+
+
+
+
+
+
+					<div class="post-preview">
+						<h2 class="post-title">
+							Répartition des amis par tranche d’âge et par sexe.
+						</h2>
+						<section class="post-meta">
+							<div id="age_sex_friends_inf"></div>
+							<div id="age_sex_friends"></div>
+						</section>
+					</div>
+
+
+					<hr>
+
+					<div class="post-preview">
+						<h2 class="post-title">
+							Pourcentage d'amis masculin et féminin
+						</h2>
+						<section class="post-meta">
+							<div class="plot" id="pourcentage_amis_masculin_feminin"></div>
+						</section>
+
+					</div>
+
+					<hr>
+
+					<div class="post-preview">
+						<h2 class="post-title">
+							Popularité du profil
+						</h2>
+						<section class="post-meta">
+							<div class="form-group">
+								<div class="radio">
+									<label>
+										<input type="radio" name="genre" id="feminin" value="feminin" checked>
+										Femmes
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="genre" id="masculin" value="masculin">
+										Hommes
+									</label>
+								</div>
+							</div>
+							<div class="plot" id="popularite_profil_femmes"></div>
+							<div class="plot" id="popularite_profil_hommes"></div>
+						</section>
+					</div>
+
+
+					<hr>
+
+					<div class="post-preview">
+						<h2 class="post-title">
+							Pourcentage de messages reçu d'amis/non-amis
+						</h2>
+						<section class="post-meta">
+							<div><span id="info31"></span></div>
+							<div class="plot" data-user-id="<?=$id?>" id="chart31"></div>
+						</section>
+					</div>
+
+					<div class="post-preview">
+						<h2 class="post-title">
+							Popularité de votre photo
+						</h2>
+						<section class="post-meta">
+							<div class="plot" data-user-id="<?=$id?>" id="chart32"></div>
+						</section>
+					</div>
+					<hr>
+				</div>
+			</div>
+			<?php include ('structure/footer.php'); ?>
 	</body>
 </html>
--- js/dataviz.js
+++ js/dataviz.js
@@ -19,4 +19,308 @@
 	getRequest("webservices/infos_user.php?user=2", function(data) {
 		console.log(data);
 	});
+
+	/*********************************************************
+	 	Graph représentant le % de messages aux amis/non amis
+	 **********************************************************/
+	var user_id =  $('#chart31').data('user-id');
+	var amis;
+	var messages;
+	jQuery.ajax({
+		type: "GET",
+		url: "/webservices/messages_user.php?user="+user_id
+	}).done(function (data) {
+		messages = $.parseJSON(data);
+		console.log(messages);
+		jQuery.ajax({
+			type: "GET",
+			url: "/webservices/liste_amis_user.php?user="+user_id
+		}).done(function (data) {
+			amis = $.parseJSON(data);
+			var messages_amis = 0;
+			var messages_non_amis = 0;
+			for (var i = 0; i < messages.length; i++) {
+				for (var j = 0; j < amis.length; j++) {
+					if ((messages[i][0] == amis[j][0] || messages[i][1] == amis[j][1]) || (messages[i][0] == amis[j][1] || messages[i][1] == amis[j][0])) {
+						messages_amis++;
+						break;
+					} else {
+						messages_non_amis++;
+						break;
+					}
+				}
+			}
+
+			$.jqplot.config.enablePlugins = true;
+			var s1 = [(messages_amis / messages.length) * 100, (messages_non_amis / messages.length) * 100];
+			var ticks = ['Messages amis', 'Messages non amis'];
+
+			plot1 = $.jqplot('chart31', [s1], {
+				animate: !$.jqplot.use_excanvas,
+				seriesDefaults: {
+					renderer: $.jqplot.BarRenderer,
+					pointLabels: {show: true, formatString: '%d'}
+				},
+				axes: {
+					xaxis: {
+						renderer: $.jqplot.CategoryAxisRenderer,
+						ticks: ticks
+					}
+				},
+				highlighter: {show: false}
+			});
+			/*****
+			 * Permet de cliquer sur les barres pour afficher des détails
+			 */
+			$('#chart31').bind('jqplotDataClick',
+				function (ev, seriesIndex, pointIndex, data) {
+					$('#info31').html('valeur: '+data[1]+' %');
+				}
+			);
+		});
+	});
+	/*************************************************************
+	 * FIN GRAPH
+	 ************************************************************/
+
+	/*********************************************************
+	 Graph représentant l'évolution de la popularité de la photo
+	 **********************************************************/
+	var user_id =  $('#chart32').data('user-id');
+	var notations;
+	jQuery.ajax({
+		type: "GET",
+		url: "/webservices/notations_user.php?user="+user_id
+	}).done(function (data) {
+		notations = $.parseJSON(data);
+		notations.sort(function(a, b) {
+			var dt1 = Date.parse(a[3]);
+			var dt2 = Date.parse(b[3]);
+
+			if (dt1 < dt2) return -1;
+			if (dt2 < dt1) return 1;
+			return 0;
+		});
+		for (var i = 0; i<notations.length; i++) {
+			notations[i].reverse();
+			if (i > 0 && notations[i][0] == notations[i-1][0]) {
+				notations[i][2] = (notations[i-1][1] + notations[i][1]) / 2;
+				delete notations[i-1];
+			}
+			delete notations[i][2];
+			delete notations[i][3];
+		}
+		notations = notations.filter(function(n){return n !== undefined});
+
+		var plot1 = $.jqplot('chart32', [notations], {
+			axes:{
+				xaxis:{
+					renderer:$.jqplot.DateAxisRenderer
+				},
+				yaxis: {
+					min: 0,
+					max: 5
+				}
+			},
+			series:[{lineWidth:4, markerOptions:{style:'square'}}]
+		});
+	});
+	/***************************************
+	 * FIN DE GRAPH
+	 ***************************************/
+
+	function generatePieChart(idDiv, data) {
+		// On génère l'exemple du TP :
+		var plot1 = $.jqplot(idDiv, [data], {
+			gridPadding: {top:0, bottom:38, left:0, right:0},
+			seriesDefaults:{
+				renderer:$.jqplot.PieRenderer,
+				trendline:{ show:false },
+				rendererOptions: { padding: 8, showDataLabels: true, sliceMargin: 6, startAngle: -90 }
+			},
+			legend:{
+				show:true,
+				placement: 'inside',
+				rendererOptions: {
+					numberRows: 3
+				},
+				location:'ne',
+				marginTop: '15px'
+			}
+		});
+	}
+
+	function generateBarChart(idDiv, data) {
+		plot1 = $.jqplot(idDiv, [data[0]], {
+			// Only animate if we're not using excanvas (not in IE 7 or IE 8)..
+			animate: !$.jqplot.use_excanvas,
+			seriesDefaults:{
+				renderer:$.jqplot.BarRenderer,
+				rendererOptions: {
+					varyBarColor: true,
+					barWidth: 35,
+				},
+				pointLabels: { show: true }
+			},
+			axes: {
+				xaxis: {
+					renderer: $.jqplot.CategoryAxisRenderer,
+					ticks: data[1]
+				}
+			},
+			highlighter: { show: false }
+		});
+	}
+
+	function generateGroupBarChart(idDiv, data) {
+		plotgroupBar = $.jqplot(idDiv, data[1], {
+			seriesDefaults: {
+				renderer:$.jqplot.BarRenderer,
+				pointLabels: { show: true, location: 'e', edgeTolerance: -15 },
+				shadowAngle: 135,
+				rendererOptions: {
+					barDirection: 'horizontal'
+				}
+			},
+			axes: {
+				yaxis: {
+					renderer: $.jqplot.CategoryAxisRenderer
+				}
+			},
+			legend: {
+				show: true,
+				location: 'ne',
+				placement: 'inside',
+				labels: data[0]
+			},
+		});
+	}
+
+	function generateDateAxis(idDiv, data) {
+		//var line1=[['2008-06-30 8:00AM',4], ['2008-7-14 8:00AM',6.5], ['2008-7-28 8:00AM',5.7], ['2008-8-11 8:00AM',9], ['2008-8-25 8:00AM',8.2]];
+		var plot2 = $.jqplot(idDiv, [data], {
+			title:'Fréquentation des donjons au cours de l\'année',
+			axes:{
+				xaxis:{
+					renderer:$.jqplot.DateAxisRenderer,
+					tickOptions:{formatString:'%#d-%m'},
+					min:'01-01',
+					max:'31-12',
+					tickInterval:'1 month'
+				}
+			},
+			series:[{lineWidth:4, markerOptions:{style:'square'}}]
+		}).replot();
+	}
+
+	/***************************************
+	 QUESTION 1 : PIE CHART : Visite par marque
+	 ****************************************/
+	getRequest("webservices/infos_user.php?user=2", function(data) {
+
+	});
+
+
+	getRequest("webservices/liste_amis_user.php?user=2", function(amis) {
+		var amis_hommes = 0;
+		var amis_femmes = 0;
+		var counter = amis.length;
+		$.each(amis,function(i,ami){
+			getRequest("webservices/infos_user?user="+ami[1],function(data_ami){
+				if(data_ami[0][7] == 0)
+				{
+					amis_femmes++;
+				}
+				else
+				{
+					amis_hommes++;
+				}
+				counter--;
+				if(counter === 0){
+					var data_tableau = [['Amis Femmes',amis_hommes],['Amis Hommes',amis_femmes]];
+					generatePieChart("pourcentage_amis_masculin_feminin", data_tableau);
+				}
+			});
+		});
+	});
+
+	getRequest("webservices/notations_user?user=22", function(amis) {
+		var un_h = 0;
+		var deux_h = 0;
+		var trois_h = 0;
+		var quatre_h = 0;
+		var cinq_h = 0;
+
+		var un_f = 0;
+		var deux_f = 0;
+		var trois_f = 0;
+		var quatre_f = 0;
+		var cinq_f = 0;
+
+		var counter = amis.length;
+		$.each(amis,function(i,ami){
+			getRequest("webservices/infos_user?user="+ami[0],function(data_ami){
+				if(data_ami[0][7] == 0)
+				{
+					switch(ami[2]){
+						case '1':
+							un_f++;
+							break;
+						case '2':
+							deux_f++;
+							break;
+						case '3':
+							trois_f++;
+							break;
+						case '4':
+							quatre_f++;
+							break;
+						case '5':
+							cinq_f++;
+							break;
+					}
+				}
+				else
+				{
+					switch(ami[2]){
+						case '1':
+							un_h++;
+							break;
+						case '2':
+							deux_h++;
+							break;
+						case '3':
+							trois_h++;
+							break;
+						case '4':
+							quatre_h++;
+							break;
+						case '5':
+							cinq_h++;
+							break;
+					}
+				}
+				counter--;
+				if(counter === 0){
+					var data_tableau_femmes = [['Un',un_f],['Deux',deux_f],['Trois',trois_f],['Quatre',quatre_f],['Cinq',cinq_f]];
+					var data_tableau_hommes = [['Un',un_h],['Deux',deux_h],['Trois',trois_h],['Quatre',quatre_h],['Cinq',cinq_h]];
+					generatePieChart("popularite_profil_femmes", data_tableau_femmes);
+					generatePieChart("popularite_profil_hommes", data_tableau_hommes);
+					$("#popularite_profil_hommes").hide();
+				}
+			});
+		});
+	});
+
+	$('input:radio[name=genre]').change(function(){
+		if(this.value === "feminin"){
+			$("#popularite_profil_femmes").show();
+			$("#popularite_profil_hommes").hide();
+		}
+		else if (this.value === "masculin") {
+			$("#popularite_profil_femmes").hide();
+			$("#popularite_profil_hommes").show();
+		}
+		console.log("fail");
+	});
 });
